01b715760e85b9539d429e9946670dbed511fb00
moeeahmed <m.ahamedbusiness@gmail.com>
2022-07-30 18:25:07 +0100

    Added JS elements to main.js file

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//DOM selectors initialisings
+const game = document.querySelector(".game");
+const boardContainer = document.querySelector(".board__container");
+const inputContainer = document.querySelector(".input__container");
+const help = document.querySelector(".help__btn");
+const stats = document.querySelector(".stats__btn");
+const input = document.querySelector(".input");
+const dropdown = document.querySelector(".dropdown-content");
+
+//Class to encapsulate app info
+class App {
+  constructor() {}
+}
+
+//Create an instance of the App class
+const app = new App();
